1a90458a37640bdfb31282e2187d5bffaedb6b4f
NerdyItalian <atava8384@gmail.com>
2015-04-29 11:11:13 -0500

    added on click and created array

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-var apikey = 'e27fa347f5134d05d053b02acb00c1153f892615'; 
+var apikey = 'e27fa347f5134d05d053b02acb00c1153f892615';
+var slideArray = [];
+
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
+	for (var i = 0; i < 8; i++){
+		slideArray[i] = results[i];
+	}
     console.log(results);
+    console.log(slideArray[1].name);
 }
 
-$(document).ready(function() {
 
-	// Start the search here!
-	search('batman');
+$(document).ready(function() {
+	$(".searchBtn").on("click", function(){
+		var searchField = $("#searchField").val();
+		$("#searchField").val('');
+		search(searchField);
+	});
+
 });
 
 // HELPER FUNCTION
